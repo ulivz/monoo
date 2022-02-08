1a510c05fc185f27c8f22d377c29c021e2c71239
xuze <xuze@bytedance.com>
2022-02-08 19:00:14 +0800

    chore: add --no-changelog option

--- src/cli.ts
+++ src/cli.ts
@@ -40,6 +40,7 @@ export function bootstrapCli() {
   cli
     .command("r", `Release your monorepo.`)
     .option("--changelog", "Whether to generate changelog.")
+    .option("--no-changelog", "Whether to ignore generate changelog.")
     .option("--dry-run", "Preview execution.")
     .option("--runInBand", "Whether to publish package in series.")
     .option("--build [build]", "Execute custom build script before release.")
